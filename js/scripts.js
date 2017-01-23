b2de4594a4b9b054e4dfc6c08aad755812c1bade
nicholas mead and said ahmed <said@>
2017-01-23 11:27:54 -0800

    add jQuery functionality to our first specification.

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Business Logic
+
+function ToDo(item,urgency,time,where) {
+  this.item = item;
+  this.urgency = urgency;
+  this.time = time;
+  this.where = where;
+
+}
+
+
+
+
+// User Logic
+
+$(document).ready(function() {
+  $("#list").submit(function(event) {
+    event.preventDefault();
+
+    var result = new ToDo($("#item").val(), $("#urgency").val(), $("#time").val(), $("#where").val());
+
+    $("#output").append("<li>" + result.item + "</li>")
+    $("#output").append("<li>" + result.urgency + "</li>")
+    $("#output").append("<li>" + result.time + "</li>")
+    $("#output").append("<li>" + result.where + "</li>")
+
+  });
+
+});
